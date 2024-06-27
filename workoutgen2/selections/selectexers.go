eb4f2f2ceebe24cc0ade1fc3f9fb2dad5fa5fa2f
jwork <jkopatsis@gmail.com>
2024-06-27 16:26:29 -0700

    allowed by group business (smart)

--- workoutgen2/selections/selectexers.go
+++ workoutgen2/selections/selectexers.go
@@ -25,22 +25,26 @@ func selectID(sum float32, exers []string, ratings map[string]float32) string {
 	return exers[int(rand.Float32()*float32(len(exers)))]
 }
 
-func getSplitIDs(sum float32, exers []string, ratings map[string]float32, exercises map[string]shared.Exercise) []string {
+func getSplitIDs(sum float32, exers []string, ratings map[string]float32, exercises map[string]shared.Exercise, ret [9][]string) []string {
 	id1 := selectID(sum, exers, ratings)
 
+	for !AllowedByGroup(ret, id1, exercises) {
+		id1 = selectID(sum, exers, ratings)
+	}
+
 	id2 := selectID(sum, exers, ratings)
-	for id1 == id2 || exercises[id1].Parent == exercises[id2].Parent {
+	for id1 == id2 || exercises[id1].Parent == exercises[id2].Parent || !AllowedByGroup(ret, id2, exercises) {
 		id2 = selectID(sum, exers, ratings)
 	}
 
 	return []string{id1, id2}
 }
 
-func getComboIDs(count int, sum float32, exers []string, ratings map[string]float32, exercises map[string]shared.Exercise) []string {
+func getComboIDs(count int, sum float32, exers []string, ratings map[string]float32, exercises map[string]shared.Exercise, existing [9][]string) []string {
 	ret := []string{}
 	for i := 0; i < count; i++ {
 		currentID := selectID(sum, exers, ratings)
-		for slices.Contains(ret, currentID) || (i > 0 && exercises[ret[i-1]].Parent == exercises[currentID].Parent) {
+		for slices.Contains(ret, currentID) || (i > 0 && exercises[ret[i-1]].Parent == exercises[currentID].Parent) || !AllowedByGroup(existing, currentID, exercises) {
 			currentID = selectID(sum, exers, ratings)
 		}
 		ret = append(ret, currentID)
@@ -78,20 +82,20 @@ func SelectExercises(types [9]string, times [9]shared.ExerciseTimes, ratings map
 
 	for i, round := range types {
 		if round == "Combo" {
-			current := getComboIDs(times[i].ComboExers, comboSum, allowedCombo, ratings, exercises)
+			current := getComboIDs(times[i].ComboExers, comboSum, allowedCombo, ratings, exercises, ret)
 			for !checkPrevRoundsByType(ret, types, current, i) {
-				current = getComboIDs(times[i].ComboExers, comboSum, allowedCombo, ratings, exercises)
+				current = getComboIDs(times[i].ComboExers, comboSum, allowedCombo, ratings, exercises, ret)
 			}
 			ret[i] = current
 		} else if round == "Split" {
-			current := getSplitIDs(splitSum, allowedSplit, ratings, exercises)
+			current := getSplitIDs(splitSum, allowedSplit, ratings, exercises, ret)
 			for !checkPrevRoundsByType(ret, types, current, i) {
-				current = getSplitIDs(splitSum, allowedSplit, ratings, exercises)
+				current = getSplitIDs(splitSum, allowedSplit, ratings, exercises, ret)
 			}
 			ret[i] = current
 		} else {
 			current := []string{selectID(normalSum, allowedNormal, ratings)}
-			for !checkPrevRoundsByType(ret, types, current, i) {
+			for !checkPrevRoundsByType(ret, types, current, i) || !AllowedByGroup(ret, current[0], exercises) {
 				current = []string{selectID(normalSum, allowedNormal, ratings)}
 			}
 			ret[i] = current
@@ -99,4 +103,18 @@ func SelectExercises(types [9]string, times [9]shared.ExerciseTimes, ratings map
 	}
 
 	return ret
+}
+
+func AllowedByGroup(ret [9][]string, id string, exercises map[string]shared.Exercise) bool {
+	if exercises[id].SinglesGroup == 0 {
+		return true
+	}
+	for _, rnd := range ret {
+		for _, rid := range rnd {
+			if exercises[id].SinglesGroup == exercises[rid].SinglesGroup {
+				return false
+			}
+		}
+	}
+	return true
 }
