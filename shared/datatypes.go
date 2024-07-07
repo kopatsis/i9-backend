5d4ce0e177a918cf03857ee4b22787a2f99c51ca
James Kopatsis <jkopatsis@gmail.com>
2024-07-06 20:21:23 -0700

    reset date in user obj now

--- shared/datatypes.go
+++ shared/datatypes.go
@@ -33,6 +33,7 @@ type User struct {
 	WOStartedCt       int                `bson:"wostartct"`    //New
 	WORatedCt         int                `bson:"completed"`    //New
 	DisplayLevel      int                `bson:"displevel"`    //New
+	ResetDate         primitive.DateTime `bson:"reset"`        //New
 }
 
 type Workout struct {
